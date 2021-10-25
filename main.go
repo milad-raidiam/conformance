b990c182a2c7f1668838b7c2eae3ddf8c39cefbb
Michael Fraser <michael.fraser@raidiam.com>
2021-10-25 18:26:47 +0100

    Updated script so multiple tabls can be generated

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -9,10 +11,33 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
-func main() {
+var (
+	Target string
+)
 
-	//Open input file
-	f, _ := os.Open("./data.csv")
+/*
+* To generate all tables - run go main.go
+* To generate phase 2 table - run go main.go -t phase2
+* To generate phase 3 table - run go main.go -t phase3
+ */
+
+func init() {
+	flag.StringVar(&Target, "t", "all", "Target Table")
+	flag.Parse()
+}
+
+func main() {
+	log.Print(Target)
+	if Target == "phase2" || Target == "all" {
+		generateFromCsv("./phase2-data.csv", "./phase2-output.txt", []string{"Organisation", "Deployment", "Consentimento API", "Dados Cadastrais (PF) API", "Dados Cadastrais (PJ) API", "Resources API", "Contas API", "Cartão de Crédito API", "Operações de Crédito - Empréstimos API", "Operações de Crédito - Financiamentos API", "Operações de Crédito - Adiantamento a Depositantes API", "Operações de Crédito - Direitos Creditórios Descontados API"})
+	}
+	if Target == "phase3" || Target == "all" {
+		generateFromCsv("./phase3-data.csv", "./phase3-output.txt", []string{"Organisation", "Payments"})
+	}
+}
+
+func generateFromCsv(inputFile string, outputFile string, headers []string) {
+	f, _ := os.Open(inputFile)
 	defer f.Close()
 
 	//Read lines from file
@@ -23,10 +48,14 @@ func main() {
 	tableOutput := &strings.Builder{}
 	table := tablewriter.NewWriter(tableOutput)
 
-	headers := []string{"Organisation", "Deployment", "Consentimento API", "Dados Cadastrais (PF) API", "Dados Cadastrais (PJ) API", "Resources API", "Contas API", "Cartão de Crédito API", "Operações de Crédito - Empréstimos API", "Operações de Crédito - Financiamentos API", "Operações de Crédito - Adiantamento a Depositantes API", "Operações de Crédito - Direitos Creditórios Descontados API"}
+	var indexHeaders []string
+
+	for index := range headers {
+		indexHeaders = append(indexHeaders, strconv.Itoa(index))
+	}
 
 	//Configure table
-	table.SetHeader([]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"})
+	table.SetHeader(indexHeaders)
 	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
 	table.SetAutoWrapText(false)
 	table.SetCenterSeparator("|")
@@ -34,7 +63,7 @@ func main() {
 	table.Render()
 
 	//Open output file
-	output, _ := os.OpenFile("./output.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0775)
+	output, _ := os.OpenFile(outputFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0775)
 
 	toWrite := tableOutput.String()
 
