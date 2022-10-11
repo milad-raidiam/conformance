eb9f93dc3842d9fc48950d41ab6424752bf7e493
PMartins03 <pedro.martins@raidiam.com>
2022-10-11 15:01:55 -0300

    added command line arguments

--- scripts/check-zip.py
+++ scripts/check-zip.py
@@ -1,21 +1,35 @@
+import sys
+sys.dont_write_bytecode = True
+import argparse
 from os import listdir
 import re
 
-
-def main():
-    api_list = ["accounts", "business", "consents", "credit-card", "financings", "invoice-financings",
-        "loans", "personal", "resources", "unarranged-overdraft"]
-    version = "2.0.1"
+def main(argv = None):
+    parser = argparse.ArgumentParser(
+        description='Checks if every language has every translation key'
+    )
+    parser.add_argument(
+        "version",
+        help="Api version."
+    )
+    parser.add_argument(
+        "apis",
+        nargs="+",
+        help="Every api to be checked by the script."
+    )
+    args = parser.parse_args(argv)
+    
+    api_list = args.apis
+    version  = args.version
     directories = [f"./submissions/functional/{api}/{version}" for api in api_list]
 
-    zips = []
-    for directory in directories:
-        zips.extend(listdir(directory))
-
-    zips = [element for element in zips if element != ".DS_Store"]
-
-    pattern = re.compile(r"^\d{8}_.+_[a-z-]+_v[12]_(0[1-9]|[12]\d|3[01])-(0[1-9]|1[012])-(20\d\d).(zip|ZIP)$")
-    wrong_zips = [file for file in zips if pattern.match(file) is None]
+    wrong_zips = []
+    for api, directory in zip(api_list, directories):
+        pattern = re.compile(r"^\d{8}_.+_(?P<api>[a-z-]+)_v[12]_(0[1-9]|[12]\d|3[01])-(0[1-9]|1[012])-(20\d\d).(zip|ZIP)$")
+        for file in listdir(directory):
+            m = pattern.match(file)
+            if (m is None or (m.group('api') != api and m.group('api') != api.title())) and file != ".DS_Store":
+                wrong_zips.append(file)
 
     if len(wrong_zips):
         print("The following zip names are wrong: " + str(wrong_zips))
